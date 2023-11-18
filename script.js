568a6152f318823a3204d7e615948ca01970f6c7
adamreaksmey <adaminiature@gmail.com>
2023-11-18 15:50:31 +0700

    update

--- script.js
+++ script.js
@@ -87,3 +87,13 @@ function switchTab(arg) {
   const content = eval(arg);
   body.html(content);
 }
+
+function revealMe(arg) {
+  const btn = $(".reveal-btn");
+  const info = $(".adam-info");
+  if (arg) {
+    btn.hide();
+    info.show();
+  }
+  return;
+}
