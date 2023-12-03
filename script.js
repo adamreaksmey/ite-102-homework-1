afc5ea4472dc9551011634f7b70d0821ec97b491
adamreaksmey <adaminiature@gmail.com>
2023-12-03 13:28:40 +0700

    some update

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ const homework1 = ``;
 const homework2 = ``;
 
 let body;
+let scrollingDiv;
+let speed = 4;
+
 $(document).ready(function () {
   body = $(".main-body");
+  scrollingDiv = $(".scrolling-div");
+  scrollText();
 });
+
+function scrollText() {
+  const initialPosition = window.innerWidth;
+  let currentPosition = initialPosition;
+  
+  function animate() {
+    currentPosition -= speed;
+
+    if (currentPosition < -scrollingDiv.width()) {
+      currentPosition = window.innerWidth;
+    }
+
+    scrollingDiv.css("transform", `translateX(${currentPosition}px)`);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
 
 function switchTab(arg) {
   const content = eval(arg);
